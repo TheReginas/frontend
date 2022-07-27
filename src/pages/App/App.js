45f79f22eac11278265a625cdb659da0645e4af1
kadijab <kadija_bell@yahoo.com>
2022-07-27 18:44:44 -0400

    Frontend update

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,20 +1,28 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
 import Login from '../Login/Login';
+import Logs from '../Logs/Logs';
+import SignUp from '../SignUp/SignUp';
+import Progress from '../Progress/Progress';
+import Navbar from '../../components/Navbar/Navbar'
 
 
 function App() {
   return (  
     <>
     <header>
+     <Navbar/>
       <h1>Work Well</h1>
     </header>
 
     <main>
     <Routes>
       <Route path= '/login' element= { <Login /> } />
+      <Route path= '/logs' element={ <Logs /> } />
+      <Route path='/signup' element={ <SignUp/>}/>
+      <Route path='/progress' element={ <Progress />}/>
     </Routes>
-
+ 
     </main>
 
 
---
+++ src/pages/Logs/Logs.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+//allow user to navigate to this page and see the full list of teams logs 
+//allow user to update,edit and delete log 
+//allow user to click into log ??
+
+class Logs extends Component {
+    render() {
+        return (
+            <>
+            <h1>Team Logs</h1>
+            <div>
+                <h2>form goes here</h2>
+                {/* to see all, create, edit, & delete. */}
+            </div>
+            </>
+        );
+    }
+}
+
+export default Logs;
