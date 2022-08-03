50816c656e7b8ef1df05cd6206a09ce7a34550fd
kadijab <kadija_bell@yahoo.com>
2022-08-02 22:22:22 -0400

    re structure frontend

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,31 +1,24 @@
 import './App.css';
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from '../Login/Login';
 import Logs from '../Logs/Logs';
 import SignUp from '../SignUp/SignUp.jsx';
-import Progress from '../Progress/Progress';
 import Navbar from '../../components/Navbar/Navbar'
-import userService from '../../utils/userService';
-
-class App extends Component {
-
-  handleLogout = () => {
-    userService.logout();
-    this.setState({ user: null });
-  }
-
-  handleSignup = () => {
-    this.setState({user: userService.getUser()});
-  }
-
-  handleLogin =()=>{
-    this.setState({user: userService.getUser()});
-  }
 
 
+function App () {
+ const [logs, setLogs]= useState ([])
 
-  render () {
+  useEffect(()=>{
+    fetch('/api')
+    .then((res)=> res.json())
+    .then(logs => setLogs(logs))
+   
+  }, []);
+  
+
+
   return (  
     <>
     <header>
@@ -35,10 +28,9 @@ class App extends Component {
 
     <main>
     <Routes>
-      <Route path= '/login' element= { <Login handleSignup={this.handleLogin}/> } />
+      <Route path= '/login' element= { <Login /> } />
       <Route path= '/logs' element={ <Logs /> } />
-      <Route path='/signup' element={ <SignUp handleLogin={this.handleSignup}/>}/>
-      <Route path='/progress' element={ <Progress />}/>
+      <Route path='/signup' element={ <SignUp />}/>
     </Routes>
  
     </main>
@@ -49,6 +41,6 @@ class App extends Component {
     </>
   );
   }
-}
+
 
 export default App;
--- src/pages/Logs/Logs.jsx
+++ src/pages/Logs/Logs.jsx
@@ -1,22 +1,36 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react'
+import ProgressBar from '../../components/ProgressBar/ProgressBar';
 
 
-//allow user to navigate to this page and see the full list of teams logs 
-//allow user to update,edit and delete log 
-//allow user to click into log ??
-//Summary Details Complete check box 
-class Logs extends Component {
-    render() {
-        return (
-            <>
-            <h1>Team Logs</h1>
-            <div>
-                <h2>form goes here</h2>
-                {/* to see all, create, edit, & delete. */}
-            </div>
-            </>
-        );
-    }
+
+const Logs = () => {
+    const [completed, setCompleted] = useState(0);
+    useEffect(() => {
+      setInterval(() => setCompleted(Math.floor(Math.random()* 100) +1 ), 2000)
+
+    }, []);
+
+  return (
+    <>
+   
+    <ProgressBar completed={completed}/>
+    <h1>Team Logs</h1>
+    <div className='container'>
+        <button className='btn'> Add New Entry</button>
+    <div>
+     
+    <form>
+        <label>Title</label>
+        <input type='text' name='title' placeholder='title'/>
+        <label>Work Notes</label>
+        <input type='text' name='description' placeholder='Work Notes'/>
+        <button>Save</button>
+    </form>
+    </div>
+    </div>
+    </>
+    
+  )
 }
 
-export default Logs;
+export default Logs
