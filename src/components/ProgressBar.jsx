fd9b2761a31b028a70a475649dacbc3702ef8398
nai2rawr <babyanaiya@gmail.com>
2022-08-04 15:47:54 -0400

    progressbar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -20,9 +20,16 @@ const Title  = styled.nav`
   color: white;
   font-size: 16px;
 `
+const Filler = styled.nav`
+   height: 100%;
+   width: ${progress}%;
+   background-Color: #1cca3d;
+   border-radius: inheriet;
+   text=aliign: right;
+   transititon: wodth 1s ease-in-out;
+`
 
-
-const progress = styled.nav`
+const Progressbar = styled.nav`
    padding: 5;
    color: white;
    font-weight: bold;
@@ -118,18 +125,11 @@ outputRange:[0.0001, 0.8, 0.0001]           })
 
     return (
     <ProgressContainer>
-        <Filler><Title>Team's Progress</Title><span class='progress'> {`${progress}%`}
-        </span></Filler></ProgressContainer>
+        <Filler><Title>Team's Progress</Title><Progressbar> {`${progress}%`}
+        </Progressbar></Filler></ProgressContainer>
   )
 }
 
-const Filler = styled.nav`
-   height: 100%;
-   width: ${progress}%;
-   background-Color: #1cca3d;
-   border-radius: inheriet;
-   text=aliign: right;
-   transititon: wodth 1s ease-in-out;
-`
+
 
 export default ProgressBar
